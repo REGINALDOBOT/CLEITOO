0f9446104279a23ca3ed5f899937a588a09b72b0
REGINALDOBOT <carlosmelao344@gmail.com>
2021-03-05 00:28:24 -0300

    First commit

---
+++ src/toinmenu.js
@@ -0,0 +1,12 @@
+const dantemenu = (prefix, pushname) => {
+    return `◪ *Comandos do DANTE*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.toinmenu = dantemenu
